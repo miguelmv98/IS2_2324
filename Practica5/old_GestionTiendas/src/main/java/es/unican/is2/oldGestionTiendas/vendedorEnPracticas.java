dfad941b05ff54c8b72e8ccbc080b20faf08b269
Miguel Monje <mmv776@alumnos.unican.es>
2024-04-17 13:29:24 +0200

    Creada carpeta Practica5, añadido proyecto sin refactorizar con codigo y primeras clases analizadas

---
+++ Practica5/old_GestionTiendas/src/main/java/es/unican/is2/oldGestionTiendas/vendedorEnPracticas.java
@@ -0,0 +1,28 @@
+package es.unican.is2.oldGestionTiendas;
+
+public class vendedorEnPracticas extends Vendedor {
+	
+	private String dni;
+	
+	/**
+	 * Retorna un nuevo vendedor en practicas
+	 * @param nombre
+	 * @param dni
+	 */
+	public vendedorEnPracticas(String nombre, String id, String dni) {
+		super(nombre, id);
+		this.dni= dni;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof vendedorEnPracticas)) 
+			return false;
+		vendedorEnPracticas v = (vendedorEnPracticas) obj;
+		return (v.getId().equals(getId()) && v.getDni().equals(getDni()));
+	}
+}
